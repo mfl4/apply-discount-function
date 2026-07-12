51b0412977bf1f8e71ae0aab684b9d40678685fc
mfl4 <91680736+mfl4@users.noreply.github.com>
2026-07-12 22:19:57 +0700

    refactor: add value range constraints to discount function

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ def apply_discount(price, discount):
 
     if type(discount) not in [int, float]:
         return "The discount should be a number"
+
+    if price <= 0:
+        return "The price should be greater than 0"
+
+    if discount < 0 or discount > 100:
+        return "The discount should be between 0 and 100"
